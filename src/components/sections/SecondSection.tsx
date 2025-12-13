b8f9fe1cb0f7c19c81e0d43b28f50420edd2c6c0
AmishaNair21 <amishamnair2002@gmail.com>
2025-12-13 22:54:33 +0530

    border lining

--- src/components/sections/SecondSection.tsx
+++ src/components/sections/SecondSection.tsx
@@ -3,7 +3,40 @@ import React from 'react'
 const SecondSection = () => {
   return (
     <div className='flex min-h-screen justify-center items-center'>
+    <div className='grid grid-cols-2 border border-blue-500 min-h-screen w-full'>
+      <div className='border border-green-500'>
+        {/* left top */}
+        <div className='border border-red-500 h-1/2 p-5'>
+          <div className='border  border-purple-500 h-full bg-purple-500'>
+          
+          </div>
+         
+        </div>
+        {/* left bottom */}
+        <div className='border border-red-500 h-1/2 p-5'>
+            <div className='border  border-purple-500 h-full bg-purple-500'>
 
+          </div>
+        </div>
+
+      </div>
+      <div className='border border-green-500'>
+        {/* right top */}
+         <div className='border border-red-500 h-1/2 p-5'>
+          <div className='border  border-purple-500 h-full bg-purple-500'>
+             
+          </div>
+        </div>
+        {/* right bottom */}
+        <div className='border border-red-500 h-1/2 p-5'>
+      <div className='border  border-purple-500 h-full bg-purple-500'>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
      
 
     </div>
